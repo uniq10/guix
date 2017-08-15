55b1a92e88f91ddbb90c99e6dc3084bb1cf84f41
Caleb Ristvedt <caleb.ristvedt@cune.org>
2017-08-15 18:18:27 -0500

    linux-container: Do not include /dev/ptmx or /dev/pts from host.
    * gnu/build/linux-container.scm:
      (mount-file-systems): Do not include /dev/ptmx or /dev/pts from host. Some
      gawk tests get stuck or fail unless a fresh devpts is used, as in the C++
      daemon.
    * guix/build/syscalls.scm:
      (personality): New procedure.
      (ADDR_NO_RANDOMIZE): New variable.
    * guix/store/build-derivations.scm: Use ADDR_NO_RANDOMIZE and personality.
      Output from a builder is now delivered via pipe so that the builder doesn't
      have access to the terminal directly or something like that.
      (remove-from-trie!): Fixed a bug causing strings to get removed from the
      trie when they shouldn't be.
      (%build-derivation): Put output-spec matching in correct order.
    * guix/store/database.scm:
      (file-closure): Now takes an optional "list-so-far" vlist of already-visited
      nodes.

--- guix/build/syscalls.scm
+++ guix/build/syscalls.scm
@@ -147,7 +147,9 @@
             utmpx-address
             login-type
             utmpx-entries
-            (read-utmpx-from-port . read-utmpx)))
+            (read-utmpx-from-port . read-utmpx)
+            personality
+            ADDR_NO_RANDOMIZE))
 
 ;;; Commentary:
 ;;;
@@ -1920,4 +1922,16 @@ entry."
     ((? bytevector? bv)
      (read-utmpx bv))))
 
-;;; syscalls.scm ends here
+(define ADDR_NO_RANDOMIZE #x0040000)
+
+(define personality
+  (let ((proc (syscall->procedure int "personality" `(,unsigned-long))))
+    (lambda (persona)
+      (let-values (((ret err) (proc persona)))
+        (if (= -1 ret)
+            (throw 'system-error "personality" "~A"
+                   (list (strerror err))
+                   (list err))
+            ret)))))
+
+;;; syscalls.scm ends here 
